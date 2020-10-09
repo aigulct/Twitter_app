4b94af685136db4bffde94a6455847be313d22bb
Aigul <aigulct@gmail.com>
2020-10-09 16:42:41 -0700

    User can stay logged in

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -14,6 +14,9 @@ class LoginViewController: UIViewController {
         let myUrl = "https://api.twitter.com/oauth/request_token"
         
         TwitterAPICaller.client?.login(url: myUrl, success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("Couldn't log in, please try again.")
@@ -26,7 +29,11 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
     /*
     // MARK: - Navigation
 
